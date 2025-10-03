cbf03403fb443640e12d113827072f1207b4d6fa
Luis <124122787+MLDLReyes@users.noreply.github.com>
2025-10-03 09:14:21 +0800

    added datasets and scripts

---
+++ scripts/app.py
@@ -0,0 +1,8 @@
+import openpyxl as xl
+
+wb = xl.load_workbook('datasets/transactions.xlsx')
+sheet = wb['Sheet1']
+
+for row in range(2, sheet.max_row + 1):
+    cell = sheet.cell(row, 3)
+    print(cell.value)
