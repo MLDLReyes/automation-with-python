e06a644b6cea345afc118582cac60edd91f41476
Munuo <manuelluisdelosreyes@gmail.com>
2025-10-03 09:30:10 +0800

    updated app.py and added result as transactions2.xlsx

--- scripts/app.py
+++ scripts/app.py
@@ -5,4 +5,8 @@ sheet = wb['Sheet1']
 
 for row in range(2, sheet.max_row + 1):
     cell = sheet.cell(row, 3)
-    print(cell.value)
+    corrected_price =  cell.value * 0.9
+    corrected_price_cell = sheet.cell(row, 4)
+    corrected_price_cell.value = corrected_price
+    
+wb.save('datasets/transactions2.xlsx')
