4019f4aba3f21e7282b8bf45ba0d87d1851c67db
Munuo <manuelluisdelosreyes@gmail.com>
2025-10-06 09:03:59 +0800

    update directories, updated app,py

--- scripts/app.py
+++ scripts/app.py
@@ -1,12 +1,28 @@
 import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
 
-wb = xl.load_workbook('datasets/transactions.xlsx')
-sheet = wb['Sheet1']
 
-for row in range(2, sheet.max_row + 1):
-    cell = sheet.cell(row, 3)
-    corrected_price =  cell.value * 0.9
-    corrected_price_cell = sheet.cell(row, 4)
-    corrected_price_cell.value = corrected_price
-    
-wb.save('datasets/transactions2.xlsx')
+def process_workbook(dataset_path, output_path):
+    wb = xl.load_workbook(dataset_path)
+    sheet = wb['Sheet1']
+
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        corrected_price =  cell.value * 0.9
+        corrected_price_cell = sheet.cell(row, 4)
+        corrected_price_cell.value = corrected_price
+        
+    values = Reference(sheet,
+                    min_row=2,
+                    max_row=sheet.max_row,
+                    min_col=4,
+                    max_col=4)
+
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'e2')
+        
+    wb.save(output_path)
+
+
+process_workbook('datasets/transactions.xlsx', 'output/transactions2.xlsx')
